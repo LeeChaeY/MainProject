9b3732129a2bf8963f9e086c431f26f5d1106ba4
LeeChaeY <chland23@gmail.com>
2023-11-24 11:10:06 +0900

    Create master 23.11.24 11:09

---
+++ z.testPJT/src/main/java/z/testPJT/domain/Product.java
@@ -0,0 +1,9 @@
+package z.testPJT.domain;
+
+public class Product {
+
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ z.testPJT/src/main/java/z/testPJT/product/B.java
@@ -0,0 +1,14 @@
+package z.testPJT.product;
+
+public class B {
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
